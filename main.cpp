18b28abf57b88620469aad2bc39b9072d770baee
theobdd <theobdd@users.noreply.github.com>
2021-05-01 16:07:01 +0200

    Définitions piquet + coordonnées

--- main.cpp
+++ main.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int nbr_piquet, max;
+    int x, y;
+
+    max=50;
+
+
+    cout << "Combien de piquet" << endl;
+    cin>>nbr_piquet;
+    while(nbr_piquet>max){
+        cout << "Trop de piquet" << endl;
+        cin>>nbr_piquet;
+    }
+
+    for(int i=0; i<nbr_piquet; i++){
+
+        cout << "Saisir coordonnees X" << endl;
+        cin>>x;
+        cout << "Saisir coordonnees X" << endl;
+        cin>>y;
+    }
+
+
+
     return 0;
 }
